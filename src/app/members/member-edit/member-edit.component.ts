01b8cb2970ab5f39aea46c17645c35aab693a375
Juantrevi <juantrevi70@gmail.com>
2023-10-28 18:09:52 +0100

    -- Loading structure --

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -43,7 +43,11 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember(){
-    this.toastr.success('Profile updated successfully');
-    this.editForm?.reset(this.member);
+    this.memberService.updateMember(this.editForm?.value).subscribe({
+      next: _ => 
+      {this.toastr.success('Profile updated successfully');
+      this.editForm?.reset(this.member);
+    }
+    });
   }
 }
