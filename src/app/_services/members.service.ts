f282218d04d808d965cef1979b8f14d6206a63c4
Juantrevi <juantrevi70@gmail.com>
2023-11-07 17:35:56 +0000

    -- saving in cache memory --

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -13,12 +13,19 @@ export class MembersService {
 
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map();
 
 
   constructor(private http: HttpClient) { }
 
 
   getMembers(userParams: UserParams){
+
+    const response = this.memberCache.get(Object.values(userParams).join('-'));
+
+    if(response){
+      return of(response);
+    }
 
     let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
 
@@ -27,7 +34,12 @@ export class MembersService {
     params = params.append('gender', userParams.gender)
     params = params.append('orderBy', userParams.orderBy)
 
-    return this.getPaginatedResults<Member[]>(this.baseUrl + 'users', params);
+    return this.getPaginatedResults<Member[]>(this.baseUrl + 'users', params).pipe(
+      map(response => {
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
+      })
+    );
   }
 
 
